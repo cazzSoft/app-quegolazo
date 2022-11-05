0fa2babbcffff4b935699b2a94b9898235001583
Cristhian Zambrano <cazzdj17@hotmail.com>
2022-11-05 17:12:49 -0500

    validacion de juegos pagados

--- SKOR/Content/js/gInicio.js
+++ SKOR/Content/js/gInicio.js
@@ -1,5 +1,5 @@
 ﻿
-
+var url = window.location.protocol + '//' + window.location.host;
 $(document).ready(function () {
     
     var countDDate = new Date(),
@@ -25,14 +25,74 @@ $(document).ready(function () {
         
     }, 1000);
 
-    
-    
-
 });
 
-function validate_juego(idj) {
-   //validar si el juego ha sido cancelado
-    console.log(idj);
-    myApp.popup('.popup-code');
-    
+//funcion para kuegos publicos
+function juego_public(id) {
+    validate_juego(id, '.popup-page');
+}
+
+//funcion para juegos privado
+function juego_private(id) {
+    //validate_code(idj, codigo)
+    validate_juego(id, '.popup-code');
+}
+
+
+//validar si el juego ha sido pagado
+function validate_juego(id,Pop) {
+    var FrmData = {
+        valor: '4',
+        idjuego: id
+    };
+
+    $.ajax({
+        url: "/Juego/JuegosPagos_ValidarJuego",
+        method: "POST",
+        async: false,
+        data: FrmData,
+        dataType: "json",
+        success: function (data) {
+
+            if (data.code == 200) {
+                console.log(id);
+                if (data.meta.resultado == 'SI') {//el juego ya esta pagado
+                    //redireccionar
+                    console.log(url);
+                    location.href = url+"/Index/Cartilla/85";
+                } else if (data.meta.resultado == 'NO') {
+                    myApp.popup(`${Pop}`);
+                }
+            }
+        },
+
+        error: function (data) {
+            console.log(data);
+        },
+    });
+}
+
+
+//validar codigo de juegos privados 
+function validate_code(idj,codigo) {
+    $.ajax({
+        url: "/cita/pacienteUpdate",
+        /*method: "POST",*/
+        data: FrmData,
+        dataType: "json",
+        success: function (data) {
+
+            if (data.length != 0) {
+                if (data) {//el juego ya esta pagado
+                    return 'true';
+                } else {
+                    return 'false';
+                }
+            }
+        },
+
+        error: function (data) {
+            console.log(data);
+        },
+    });
 }
