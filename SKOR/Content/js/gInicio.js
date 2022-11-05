74dac9d769b9005b8ef48215844c9efbffa0e303
Cristhian Zambrano <cazzdj17@hotmail.com>
2022-11-05 10:37:28 -0500

    cronometro

--- SKOR/Content/js/gInicio.js
+++ SKOR/Content/js/gInicio.js
@@ -1,8 +1,28 @@
 ﻿
 
 $(document).ready(function () {
-    $('#prueba').click(function () {
-        alert(); 
-        $('#exampleModal').modal('show');
-    });
+    
+    var countDDate = new Date(),
+    endday = new Date(), days = 17;
+    countDDate.setDate(countDDate.getDate() - (countDDate.getDay() + 2) % 7);
+    endday.setDate(countDDate.getDate() + days);
+    endday.getTime();
+    countDiv = document.getElementById('countdown');
+
+
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var daterest = endday - now;
+        var days = Math.floor(daterest / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((daterest % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes = Math.floor((daterest % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds = Math.floor((daterest % (1000 * 60)) / 1000);
+        if (days == 0) {
+            $('.class_').remove();
+        } else {
+            countDiv.innerHTML = days + " diás " + hours + ":" + minutes + ":" + seconds + " ";
+        }
+        
+    }, 1000);
+
 });
