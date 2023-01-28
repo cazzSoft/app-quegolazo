9544363d6f24bdb8b78a4c907a12afaec3fd59de
Cristhian Zambrano <cazzdj17@hotmail.com>
2023-01-28 16:52:44 -0500

    update config

--- SKOR/Models/JUEGOS/dbConnection.cs
+++ SKOR/Models/JUEGOS/dbConnection.cs
@@ -8,7 +8,7 @@ namespace Skor.Models.JUEGOS
 {
 	public class dbConnection
 	{
-        private string connectionString = "data source=ANONYMOUS;initial catalog=skor-2021-10-6-12-5;persist security info=True;user id=sa;password=12345678;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+        private string connectionString = "data source=crms.database.windows.net;initial catalog=skor-2021-10-6-12-5;persist security info=True;user id=crms;password=vertice.123;MultipleActiveResultSets=True;App=EntityFramework&quot;";
 
         //private string connectionString = "data source=192.168.100.99;initial catalog=skor-2021-10-6-12-5;persist security info=True;user id=sa;password=adstecnologia2022;MultipleActiveResultSets=True;App=EntityFramework&quot;";
 
